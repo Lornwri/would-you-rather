fd1f651c2cf747ae80622970be9a9a8972d60ddb
Lornwri <34406133+Lornwri@users.noreply.github.com>
2024-11-10 23:46:53 -0600

    Initial post
    Signed-off-by: Lornwri <34406133+Lornwri@users.noreply.github.com>

--- src/components/WouldYouRather.vue
+++ src/components/WouldYouRather.vue
@@ -9,31 +9,57 @@ defineProps({
  answer2: String,
 })
 
+const emit = defineEmits(['answer-selected'])
+
 const choice = ref('')
+
+function choiceMade() {
+  emit('answer-selected', choice.value)}
 
 </script>
 
 <template>
  
+  <div id="wyr">
+    
   <h2>Make your choice!</h2>
-  
+    
+    <div id="question">
   {{question}}
-  
+    </div>
+    
+    <div id="answers">
   <br>
   <br>
-  
   <!--Create buttons for answers using HTML value -->
   <input v-model="choice" v-bind:value="answer1" type="radio" id="answer-1">
   <label for="answer-1">{{answer1}}</label>
   
   <input v-model="choice" v-bind:value="answer2" type="radio" id="answer-2">
   <label for="answer-2">{{answer2}}</label>
-  
+    </div>
+    
+  </div>
   
 </template>
 
 <style scoped>
 
+#wyr {
+  background-color: brown;
+  padding: 10px;
+  width: 500px;
+  height: 500px;
+  border: 1px solid black;
+  border-radius: 10px;
+  text-align: center;
+  font-size: 20px;
+}
 
-
+#answers {
+  padding: 10px;
+  background-color: brown;
+  border: 2px solid black;
+  border-radius: 10px;
+}
 </style>
