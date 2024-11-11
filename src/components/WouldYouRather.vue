528fc5e64de36666ca8e2a548403a4260bde5d13
Lornwri <34406133+Lornwri@users.noreply.github.com>
2024-11-10 23:13:58 -0600

    Initial post
    Signed-off-by: Lornwri <34406133+Lornwri@users.noreply.github.com>

--- src/components/WouldYouRather.vue
+++ src/components/WouldYouRather.vue
@@ -28,11 +28,10 @@ const choice = ref('')
   
   <input v-model="choice" v-bind:value="answer2" type="radio" id="answer-2">
   <label for="answer-2">{{answer2}}</label>
-  
   <br>
   <br>
   
-  <button v-on:click="$emit('answer', choice)">Submit</button>
+  <button v-on:click="$emit('answer-selected', choice)">Submit</button>
   
   
 </template>
