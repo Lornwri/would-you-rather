5b995960506cead6dcac76f085052005ad480c56
Lornwri <34406133+Lornwri@users.noreply.github.com>
2024-11-10 23:11:42 -0600

    Initial post
    Signed-off-by: Lornwri <34406133+Lornwri@users.noreply.github.com>

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import WouldYouRather from './components/WouldYouRather.vue'
+
+import {ref} from 'vue'
+
+const wyrQuestion = ref('The aliens that make first contact be robotic or organic?')
+const wyrAnswer1 = ref('Organic')
+const wyrAnswer2 = ref('Robotic')
+
+//stores user's answers
+
+const userSelection = ref('') //empty string for user's answer
+
+</script>
+
+<template>
+  
+  <h1>Would You Rather?</h1>
+  
+  <WouldYouRather v-bind:question="wyrQuestion" 
+                  v-bind:answer1="wyrAnswer1" 
+                  v-bind:answer2="wyrAnswer2">
+  </WouldYouRather>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/WouldYouRather.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue'
+
+
+//Define data
+defineProps({
+ question: String,
+ answer1: String,
+ answer2: String,
+})
+
+const choice = ref('')
+
+</script>
+
+<template>
+ 
+  <h2>Make your choice!</h2>
+  
+  {{question}}
+  
+  <br>
+  <br>
+  
+  <!--Create buttons for answers using HTML value -->
+  <input v-model="choice" v-bind:value="answer1" type="radio" id="answer-1">
+  <label for="answer-1">{{answer1}}</label>
+  
+  <input v-model="choice" v-bind:value="answer2" type="radio" id="answer-2">
+  <label for="answer-2">{{answer2}}</label>
+  
+  
+</template>
+
+<style scoped>
+
+
+
+</style>
